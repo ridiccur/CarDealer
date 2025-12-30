9b8796b3a4ccf45d1fd0c36c34ff784a78c4c878
Илатовский Данил Алексеевич <heafraer@gmail.com>
2025-12-30 10:53:09 +0300

    Add filtering and searching by any field

--- CarDealer/Form1.Designer.cs
+++ CarDealer/Form1.Designer.cs
@@ -28,48 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-
+            dataGridView1 = new DataGridView();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            cbFilterField = new ComboBox();
+            cbFilterOperator = new ComboBox();
+            txtFilterValue = new TextBox();
+            btnAddFilter = new Button();
+            lstFilters = new ListBox();
+            btnApplyFilters = new Button();
+            btnClearFilters = new Button();
+            txtSearch = new TextBox();
+            cbSearchField = new ComboBox();
+            btnSearch = new Button();
+            btnClearSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
             // dataGridView1
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(960, 300);
-            this.dataGridView1.TabIndex = 0;
-
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(15, 30);
+            dataGridView1.Margin = new Padding(6, 7, 6, 7);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.Size = new Size(1767, 738);
+            dataGridView1.TabIndex = 0;
+            // 
             // btnAdd
-            this.btnAdd.Location = new System.Drawing.Point(12, 330);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "Добавить";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-
+            // 
+            btnAdd.Location = new Point(27, 819);
+            btnAdd.Margin = new Padding(6, 7, 6, 7);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(162, 57);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Добавить";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
             // btnDelete
-            this.btnDelete.Location = new System.Drawing.Point(97, 330);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 2;
-            this.btnDelete.Text = "Удалить";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-
+            // 
+            btnDelete.Location = new Point(27, 883);
+            btnDelete.Margin = new Padding(6, 7, 6, 7);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(162, 57);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Удалить";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // cbFilterField
+            // 
+            cbFilterField.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbFilterField.FormattingEnabled = true;
+            cbFilterField.Location = new Point(847, 784);
+            cbFilterField.Margin = new Padding(6, 7, 6, 7);
+            cbFilterField.Name = "cbFilterField";
+            cbFilterField.Size = new Size(204, 40);
+            cbFilterField.TabIndex = 3;
+            // 
+            // cbFilterOperator
+            // 
+            cbFilterOperator.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbFilterOperator.FormattingEnabled = true;
+            cbFilterOperator.Location = new Point(1069, 784);
+            cbFilterOperator.Margin = new Padding(6, 7, 6, 7);
+            cbFilterOperator.Name = "cbFilterOperator";
+            cbFilterOperator.Size = new Size(226, 40);
+            cbFilterOperator.TabIndex = 4;
+            cbFilterOperator.SelectedIndexChanged += cbFilterOperator_SelectedIndexChanged;
+            // 
+            // txtFilterValue
+            // 
+            txtFilterValue.Location = new Point(1307, 784);
+            txtFilterValue.Margin = new Padding(6, 7, 6, 7);
+            txtFilterValue.Name = "txtFilterValue";
+            txtFilterValue.Size = new Size(230, 39);
+            txtFilterValue.TabIndex = 5;
+            // 
+            // btnAddFilter
+            // 
+            btnAddFilter.Location = new Point(1549, 779);
+            btnAddFilter.Margin = new Padding(6, 7, 6, 7);
+            btnAddFilter.Name = "btnAddFilter";
+            btnAddFilter.Size = new Size(217, 57);
+            btnAddFilter.TabIndex = 6;
+            btnAddFilter.Text = "Добавить фильтр";
+            btnAddFilter.UseVisualStyleBackColor = true;
+            btnAddFilter.Click += btnAddFilter_Click;
+            // 
+            // lstFilters
+            // 
+            lstFilters.FormattingEnabled = true;
+            lstFilters.Location = new Point(847, 846);
+            lstFilters.Margin = new Padding(6, 7, 6, 7);
+            lstFilters.Name = "lstFilters";
+            lstFilters.Size = new Size(690, 132);
+            lstFilters.TabIndex = 7;
+            lstFilters.SelectedIndexChanged += lstFilters_SelectedIndexChanged;
+            lstFilters.DoubleClick += lstFilters_DoubleClick;
+            // 
+            // btnApplyFilters
+            // 
+            btnApplyFilters.Location = new Point(1549, 850);
+            btnApplyFilters.Margin = new Padding(6, 7, 6, 7);
+            btnApplyFilters.Name = "btnApplyFilters";
+            btnApplyFilters.Size = new Size(217, 57);
+            btnApplyFilters.TabIndex = 8;
+            btnApplyFilters.Text = "Применить";
+            btnApplyFilters.UseVisualStyleBackColor = true;
+            btnApplyFilters.Click += btnApplyFilters_Click;
+            // 
+            // btnClearFilters
+            // 
+            btnClearFilters.Location = new Point(1549, 921);
+            btnClearFilters.Margin = new Padding(6, 7, 6, 7);
+            btnClearFilters.Name = "btnClearFilters";
+            btnClearFilters.Size = new Size(217, 57);
+            btnClearFilters.TabIndex = 9;
+            btnClearFilters.Text = "Очистить";
+            btnClearFilters.UseVisualStyleBackColor = true;
+            btnClearFilters.Click += btnClearFilters_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(212, 828);
+            txtSearch.Margin = new Padding(6, 7, 6, 7);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(429, 39);
+            txtSearch.TabIndex = 10;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // cbSearchField
+            // 
+            cbSearchField.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbSearchField.FormattingEnabled = true;
+            cbSearchField.Location = new Point(212, 892);
+            cbSearchField.Margin = new Padding(6, 7, 6, 7);
+            cbSearchField.Name = "cbSearchField";
+            cbSearchField.Size = new Size(429, 40);
+            cbSearchField.TabIndex = 11;
+            cbSearchField.SelectedIndexChanged += cbSearchField_SelectedIndexChanged;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(663, 819);
+            btnSearch.Margin = new Padding(6, 7, 6, 7);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(156, 57);
+            btnSearch.TabIndex = 12;
+            btnSearch.Text = "Поиск";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // btnClearSearch
+            // 
+            btnClearSearch.Location = new Point(663, 883);
+            btnClearSearch.Margin = new Padding(6, 7, 6, 7);
+            btnClearSearch.Name = "btnClearSearch";
+            btnClearSearch.Size = new Size(156, 57);
+            btnClearSearch.TabIndex = 13;
+            btnClearSearch.Text = "Сброс";
+            btnClearSearch.UseVisualStyleBackColor = true;
+            btnClearSearch.Click += btnClearSearch_Click;
+            // 
             // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.btnDelete);
-            this.Name = "Form1";
-            this.Text = "Информационная система автосалона";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1797, 1010);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnAdd);
+            Controls.Add(btnDelete);
+            Controls.Add(cbFilterField);
+            Controls.Add(cbFilterOperator);
+            Controls.Add(txtFilterValue);
+            Controls.Add(btnAddFilter);
+            Controls.Add(lstFilters);
+            Controls.Add(btnApplyFilters);
+            Controls.Add(btnClearFilters);
+            Controls.Add(txtSearch);
+            Controls.Add(cbSearchField);
+            Controls.Add(btnSearch);
+            Controls.Add(btnClearSearch);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "Form1";
+            Text = "Информационная система автосалона";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -77,5 +224,18 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
+
+        private System.Windows.Forms.ComboBox cbFilterField;
+        private System.Windows.Forms.ComboBox cbFilterOperator;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.Button btnAddFilter;
+        private System.Windows.Forms.ListBox lstFilters;
+        private System.Windows.Forms.Button btnApplyFilters;
+        private System.Windows.Forms.Button btnClearFilters;
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cbSearchField;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClearSearch;
     }
 }
--- CarDealer/Form1.cs
+++ CarDealer/Form1.cs
@@ -9,14 +9,40 @@ namespace CarDealer
     public partial class Form1 : Form
     {
         private BindingList<Car> cars = new BindingList<Car>();
+        private List<Car> allCars = new List<Car>();
 
         private string lastSortedColumn = null;
         private bool isAscending = true;
+
+        private static readonly Dictionary<string, string> OperatorDisplayMap = new Dictionary<string, string>
+        {
+            { "Contains", "Содержит" },
+            { "Equals", "Равно" },
+            { "GreaterThan", "Больше" },
+            { "LessThan", "Меньше" },
+            { "GreaterOrEqual", "Больше или равно" },
+            { "LessOrEqual", "Меньше или равно" }
+        };
+
+        private class FilterCriterion
+        {
+            public string Field { get; set; }
+            public string Operator { get; set; }
+            public string Value { get; set; }
+            public override string ToString()
+            {
+                var op = OperatorDisplayMap.TryGetValue(Operator, out var disp) ? disp : Operator;
+                return $"{Field} {op} {Value}";
+            }
+        }
+
+        private readonly List<FilterCriterion> filters = new List<FilterCriterion>();
 
         public Form1()
         {
             InitializeComponent();
             SetupDataGridView();
+            SetupFilterControls();
             LoadSampleData();
         }
 
@@ -95,17 +121,54 @@ namespace CarDealer
             dataGridView1.ColumnHeaderMouseClick += DataGridView1_ColumnHeaderMouseClick;
         }
 
+        // Настройка элементов фильтра/поиска
+        private void SetupFilterControls()
+        {
+            var properties = new[] { "Brand", "Model", "Year", "InStock", "Count", "Trim", "Price", "Discounts" };
+            cbFilterField.Items.Clear();
+            cbSearchField.Items.Clear();
+
+            cbSearchField.Items.Add("Любое");
+
+            foreach (var p in properties)
+            {
+                cbFilterField.Items.Add(p);
+                cbSearchField.Items.Add(p);
+            }
+
+            if (cbFilterField.Items.Count > 0) cbFilterField.SelectedIndex = 0;
+            if (cbSearchField.Items.Count > 0) cbSearchField.SelectedIndex = 0;
+
+            var operatorKeys = new[] { "Contains", "Equals", "GreaterThan", "LessThan", "GreaterOrEqual", "LessOrEqual" };
+            var operatorLabelsRus = operatorKeys.Select(k => OperatorDisplayMap[k]).ToArray();
+
+            cbFilterOperator.Items.Clear();
+            cbFilterOperator.Items.AddRange(operatorLabelsRus);
+            cbFilterOperator.SelectedIndex = 0;
+        }
+
         // Начальные данные
         private void LoadSampleData()
         {
-            cars.Add(new Car("Toyota", "Camry", 2024, true, 5, "Comfort", 2_500_000, "Trade-in"));
-            cars.Add(new Car("BMW", "X5", 2025, false, 0, "M Sport", 8_500_000, "Military"));
-            cars.Add(new Car("Hyundai", "Tucson", 2023, true, 3, "Premium", 2_200_000, ""));
-            cars.Add(new Car("Lada", "Granta", 2024, true, 10, "Classic", 750_000, "Госпрограмма"));
-            cars.Add(new Car("Lada", "Vesta", 2025, true, 7, "Comfort", 1_100_000, "Господдержка"));
-            cars.Add(new Car("Lada", "Niva Travel", 2023, true, 4, "Luxury", 1_300_000, "Семейная"));
+            allCars.Clear();
+            allCars.Add(new Car("Toyota", "Camry", 2024, true, 5, "Comfort", 2_500_000, "Trade-in"));
+            allCars.Add(new Car("BMW", "X5", 2025, false, 0, "M Sport", 8_500_000, "Ипотечная скидка"));
+            allCars.Add(new Car("Hyundai", "Tucson", 2023, true, 3, "Premium", 2_200_000, ""));
+            allCars.Add(new Car("Lada", "Granta", 2024, true, 10, "Classic", 750_000, "Госпрограмма"));
+            allCars.Add(new Car("Lada", "Vesta", 2025, true, 7, "Comfort", 1_100_000, "Господдержка"));
+            allCars.Add(new Car("Lada", "Niva Travel", 2023, true, 4, "Luxury", 1_300_000, "Семейная"));
 
+            ReloadBinding(allCars);
             UpdateHeaders();
+        }
+
+        private void ReloadBinding(IEnumerable<Car> source)
+        {
+            cars.RaiseListChangedEvents = false;
+            cars.Clear();
+            foreach (var c in source) cars.Add(c);
+            cars.RaiseListChangedEvents = true;
+            cars.ResetBindings();
         }
 
         private void DataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -114,7 +177,6 @@ namespace CarDealer
             var property = TypeDescriptor.GetProperties(typeof(Car))[columnName];
             if (property == null) return;
 
-            // Инвертирование порядка сортировки при повторном клике на тот же столбец
             if (lastSortedColumn == columnName)
             {
                 isAscending = !isAscending;
@@ -169,23 +231,231 @@ namespace CarDealer
         private void btnAdd_Click(object sender, EventArgs e)
         {
             var newCar = new Car("Новый", "Автомобиль", DateTime.Now.Year, false, 0, "Base", 1_000_000, "");
+            allCars.Add(newCar);
             cars.Add(newCar);
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            Car carToRemove = null;
             if (dataGridView1.SelectedRows.Count > 0)
             {
                 var selectedRow = dataGridView1.SelectedRows[0];
-                var car = (Car)selectedRow.DataBoundItem;
-                cars.Remove(car);
+                carToRemove = (Car)selectedRow.DataBoundItem;
             }
             else if (dataGridView1.CurrentCell != null)
             {
                 var row = dataGridView1.CurrentCell.OwningRow;
-                var car = (Car)row.DataBoundItem;
-                cars.Remove(car);
+                carToRemove = (Car)row.DataBoundItem;
             }
+
+            if (carToRemove != null)
+            {
+                allCars.Remove(carToRemove);
+                cars.Remove(carToRemove);
+            }
+        }
+
+        // Добавление фильтров
+        private void btnAddFilter_Click(object sender, EventArgs e)
+        {
+            if (cbFilterField.SelectedItem == null || cbFilterOperator.SelectedItem == null) return;
+
+            var operatorKeys = new[] { "Contains", "Equals", "GreaterThan", "LessThan", "GreaterOrEqual", "LessOrEqual" };
+            var opIndex = cbFilterOperator.SelectedIndex;
+            if (opIndex < 0 || opIndex >= operatorKeys.Length) return;
+            var opKey = operatorKeys[opIndex];
+
+            var criterion = new FilterCriterion
+            {
+                Field = cbFilterField.SelectedItem.ToString(),
+                Operator = opKey,
+                Value = txtFilterValue.Text ?? string.Empty
+            };
+            filters.Add(criterion);
+            lstFilters.Items.Add(criterion.ToString());
+            txtFilterValue.Clear();
+        }
+
+        // Удаление элемента из списка фильтров
+        private void lstFilters_DoubleClick(object sender, EventArgs e)
+        {
+            var idx = lstFilters.SelectedIndex;
+            if (idx >= 0 && idx < filters.Count)
+            {
+                filters.RemoveAt(idx);
+                lstFilters.Items.RemoveAt(idx);
+            }
+        }
+
+        // Применить фильтры
+        private void btnApplyFilters_Click(object sender, EventArgs e)
+        {
+            ApplyFilters();
+        }
+
+        private void ApplyFilters()
+        {
+            IEnumerable<Car> query = allCars;
+
+            foreach (var f in filters)
+            {
+                query = ApplySingleFilter(query, f);
+            }
+
+            ReloadBinding(query);
+        }
+
+        private IEnumerable<Car> ApplySingleFilter(IEnumerable<Car> source, FilterCriterion f)
+        {
+            if (string.IsNullOrEmpty(f.Value)) return source;
+
+            var prop = TypeDescriptor.GetProperties(typeof(Car))[f.Field];
+            if (prop == null) return source;
+
+            var type = prop.PropertyType;
+
+            Func<Car, bool> predicate = car =>
+            {
+                var raw = prop.GetValue(car);
+                if (raw == null) return false;
+
+                var rawStr = raw.ToString() ?? string.Empty;
+                var valStr = f.Value;
+
+                switch (f.Operator)
+                {
+                    case "Contains":
+                        return rawStr.IndexOf(valStr, StringComparison.OrdinalIgnoreCase) >= 0;
+                    case "Equals":
+                        if (type == typeof(string))
+                        {
+                            return string.Equals(rawStr, valStr, StringComparison.OrdinalIgnoreCase);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                var converted = Convert.ChangeType(valStr, type);
+                                return object.Equals(raw, converted);
+                            }
+                            catch { return false; }
+                        }
+                    case "GreaterThan":
+                        try
+                        {
+                            var converted = Convert.ChangeType(valStr, type);
+                            return Comparer<object>.Default.Compare(raw, converted) > 0;
+                        }
+                        catch { return false; }
+                    case "LessThan":
+                        try
+                        {
+                            var converted = Convert.ChangeType(valStr, type);
+                            return Comparer<object>.Default.Compare(raw, converted) < 0;
+                        }
+                        catch { return false; }
+                    case "GreaterOrEqual":
+                        try
+                        {
+                            var converted = Convert.ChangeType(valStr, type);
+                            return Comparer<object>.Default.Compare(raw, converted) >= 0;
+                        }
+                        catch { return false; }
+                    case "LessOrEqual":
+                        try
+                        {
+                            var converted = Convert.ChangeType(valStr, type);
+                            return Comparer<object>.Default.Compare(raw, converted) <= 0;
+                        }
+                        catch { return false; }
+                    default:
+                        return false;
+                }
+            };
+
+            return source.Where(predicate);
+        }
+
+        private void btnClearFilters_Click(object sender, EventArgs e)
+        {
+            filters.Clear();
+            lstFilters.Items.Clear();
+            ReloadBinding(allCars);
+        }
+
+        // Поиск
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            ApplySearch();
+        }
+
+        private void ApplySearch()
+        {
+            var q = txtSearch.Text?.Trim();
+            if (string.IsNullOrEmpty(q))
+            {
+                ReloadBinding(allCars);
+                return;
+            }
+
+            var selectedField = cbSearchField.SelectedItem?.ToString() ?? "Любое";
+
+            IEnumerable<Car> result;
+            if (selectedField == "Любое")
+            {
+                result = allCars.Where(car =>
+                {
+                    foreach (PropertyDescriptor pd in TypeDescriptor.GetProperties(typeof(Car)))
+                    {
+                        var val = pd.GetValue(car);
+                        if (val != null && val.ToString().IndexOf(q, StringComparison.OrdinalIgnoreCase) >= 0) return true;
+                    }
+                    return false;
+                });
+            }
+            else
+            {
+                var prop = TypeDescriptor.GetProperties(typeof(Car))[selectedField];
+                if (prop == null) { result = Enumerable.Empty<Car>(); }
+                else
+                {
+                    result = allCars.Where(car =>
+                    {
+                        var val = prop.GetValue(car);
+                        return val != null && val.ToString().IndexOf(q, StringComparison.OrdinalIgnoreCase) >= 0;
+                    });
+                }
+            }
+
+            ReloadBinding(result);
+        }
+
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            if (cbSearchField.Items.Count > 0) cbSearchField.SelectedIndex = 0;
+            ReloadBinding(allCars);
+        }
+
+        private void cbSearchField_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbFilterOperator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstFilters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
